f6401c9a92954093f7fdafd99008f6a0ad999403
divine-mercyx <divinewilson766@gmail.com>
2025-05-21 14:11:07 +0100

    update on expense tracker backend cors policy

--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userController = require("./controllers/userController");
 const productController = require("./controllers/productController");
-const port = process.env.PORT || 3000;
 
 dotenv.config();
+const port = process.env.PORT || 3000;
+
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://expensetrackerfrontend-dkgq.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Range", "X-Content-Range"],
+  maxAge: 600,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Range", "X-Content-Range"],
+  maxAge: 600,
+}));
 app.use(express.json());
 
 app.use("/api/users", userController);
