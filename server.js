b93b90e64433209b97858eb0f59727eb35f89ed7
divine-mercyx <divinewilson766@gmail.com>
2025-05-22 07:11:39 +0100

    update on cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors({
-  origin: "https://expensetrackerfrontend-dkgq.onrender.com",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
